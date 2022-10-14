82b613a7ccb7347213cd1fe024c92ca5c04e724a
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-10-14 16:02:33 +0200

    pokemon added

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let kantoPokemonArray = [];
+
+async function displayPokemons() {
+  let firstPokemons = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=151"
+  );
+
+  let kantoPokemons = await firstPokemons.json();
+  console.log(kantoPokemons);
+
+  for (i = 0; i < kantoPokemons.results.length; i++) {
+    let pokemonURL = await fetch(kantoPokemons.results[i].url);
+    //console.log(pokemon);
+    let kantoPokemonURL = await pokemonURL.json();
+
+    kantoPokemonArray.push(kantoPokemonURL);
+    //console.log(kantoPokemonURL);
+  }
+
+  for (i = 0; i < kantoPokemonArray.length; i++) {
+    let eachPokemon = kantoPokemonArray[i];
+    if (eachPokemon.types[1] !== undefined) {
+      document.querySelector("#pokedex").innerHTML += `<div class="card col-4">
+          <img src="${eachPokemon.sprites.front_default}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${eachPokemon.name}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${eachPokemon.moves[0].move.name}</li>
+            <li class="list-group-item">${eachPokemon.types[0].type.name}</li>
+            <li class="list-group-item">${eachPokemon.types[1].type.name}</li>
+          </ul>
+        </div>`;
+    } else {
+      document.querySelector("#pokedex").innerHTML += `<div class="card col-4">
+          <img src="${eachPokemon.sprites.front_default}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${eachPokemon.name}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${eachPokemon.moves[0].move.name}</li>
+            <li class="list-group-item">${eachPokemon.types[0].type.name}</li>
+            
+          </ul>
+        </div>`;
+    }
+  }
+}
+
+console.log(kantoPokemonArray);
+
+window.onload = () => {
+  displayPokemons();
+};
